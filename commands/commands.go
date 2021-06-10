2120db9914ae7e4cdb136c1e802ade7adc0eafd6
Faisa <75602020+justfaisa@users.noreply.github.com>
2021-06-10 11:16:17 +0700

    Update commands.go

--- commands/commands.go
+++ commands/commands.go
@@ -20,46 +20,7 @@ func RegisterTwitchCommands(bot *dc.Bot) {
 	bot.CmdHandler.AddCommand(channel)
 }
 
-// RegisterFunCommands registers the fun command group
-func RegisterFunCommands(bot *dc.Bot) {
-
-}
-
-// RegisterSpacexCommands registers the spacex commands group
-func RegisterSpacexCommands(bot *dc.Bot) {
-	nextLaunch := dc.NewCommand("nextlaunch", "When is the next SpaceX launch?", NextLaunchCommand)
-	rocketInfo := dc.NewCommand("rocket", "Get some basic information about a SpaceX Rocket!", RocketInformationCommand)
-	rocketInfo.AddParameter("name", "Name of the rocket... with lowercase and no spaces.. Try: falcon9", true)
-
-	bot.CmdHandler.AddCommand(nextLaunch)
-	bot.CmdHandler.AddCommand(rocketInfo)
-}
-
-// RegisterImageCommands registers the fun command group
-func RegisterImageCommands(bot *dc.Bot) {
-
-}
-
-// RegisterUtilityCommands registers the fun command group
-func RegisterUtilityCommands(bot *dc.Bot) {
-	helpCommand := dc.NewCommand("help", "Displays help messages.", HelpCommand)
-	helpCommand.AddParameter("command", "Gets the help message for the command given, if there is one.", false)
-
-	bot.CmdHandler.AddCommand(helpCommand)
-}
-
-// RegisterTestingCommands for testing stuff
-func RegisterTestingCommands(bot *dc.Bot) {
-	macro := dc.NewCommand("macro", "Defines a macro. WIP", MacroCommand)
-	macro.AddParameter("name", "Name of macro.", true)
-	macro.AddParameter("cmd1", "cmd1", false)
-	macro.AddParameter("cmd2", "cmd2", false)
-	macro.AddParameter("cmd3", "cmd3", false)
-
-	bot.CmdHandler.AddCommand(macro)
-}
-
-// RegisterRandomCommands registers the fun command group
+// RegisterRandomCommands registers the random command group
 func RegisterRandomCommands(bot *dc.Bot) {
 	ping := dc.NewCommand("ping", "Ping!", PingPongCommand)
 	pong := dc.NewCommand("pong", "Pong!", PingPongCommand)
@@ -74,3 +35,7 @@ func RegisterRandomCommands(bot *dc.Bot) {
 	bot.CmdHandler.AddCommand(ping)
 	bot.CmdHandler.AddCommand(pong)
 }
+
+// RegisterMusicCommands register the music command group
+func RegisterMusicCommands(bot *dc.Bot) {
+
